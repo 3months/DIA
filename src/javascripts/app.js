5a12565fcae5303fb93f797e42af2bf5110f7535
Tinus Wagner <tinus@3months.com>
2017-06-12 14:37:43 +1200

    making progress

--- src/javascripts/app.js
+++ src/javascripts/app.js
@@ -13,7 +13,6 @@ function recursiveLoop(obj) {
   for (var k in obj) {
     var i = 0;
     if (obj[k].hasOwnProperty("name")) {
-      // console.log(obj[k])
       createDiv(obj[k], i)
       recursiveLoop(obj[k])
     }
@@ -32,27 +31,37 @@ function recursiveLoop(obj) {
 // Create a new div for each business Rule
 function createDiv(obj, counter){
   text = obj.name
-  $("#list").append("<div class='col-lg-3'><div class='panel panel-default' id="+ text+"><div class='panel-heading'>" + "<h2 class='panel-title'> <a data-toggle='collapse' href='#" + text+counter + "'>"+ text+ "</a></h2></div>" + "</div>")
+  var title = returnTitle(text)
+  $("#list").append("<div class='col-lg-3'><div class='panel panel-default' id="+ text+"><div class='panel-heading'>" + "<h2 class='panel-title'> <a data-toggle='collapse' href='#" + text+counter + "'>"+ title+ "</a></h2></div>" + "</div>")
 }
 
 // Create a new child element for nested properties
 function createChildElement(obj, child, counter){
   var parent = document.getElementById(obj.name)
-  console.log(obj)
   $(parent).append("<div id="+obj.name+ counter+" class='panel-collapse collapse'><div class='panel-body' id='panel"+obj.name+ counter+"'></div></div></div>")
   for (var key in child) {
-    console.log(key)
     var panel_body_id = "panel" + obj.name+counter
     var panel_body = document.getElementById(panel_body_id)
     if (child.hasOwnProperty(key)) {
-      var text = "<p>" + key + ':' + child[key] + "</p>"
+      var text = "<p>" + returnRequirementKey(key) + ' :<strong>' + child[key] + "</strong></p>"
       $(panel_body).append(text)
     }
   }
 }
 
-function appendToParent(obj){
+function returnTitle(text){
+  return text.replace(/([a-z])([A-Z])/g, '$1 $2')
+}
 
+function returnRequirementKey(text){
+  switch (text) {
+    case "applicantMinimumAge": return "Minimum age of applicant"
+    case "yearsInNzSince20": return "Years applicant should have spent in NZ since turning 20"
+    case "yearsInNzSince50": return "Years applicant should have spent in NZ since turning 50"
+    case "citizenOrResident?": return "Is the applicant a citizen or Resident of NZ?"
+    case "livingInNZ?": return "Is the applicant currently living in NZ"
+    default: return text
+  }
 }
 
 // $(function() {
