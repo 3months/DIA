789225e31452d3342007e07ce4010a3afa9d7a7f
Tinus Wagner <tinus@3months.com>
2017-06-12 13:59:46 +1200

    working

--- src/javascripts/app.js
+++ src/javascripts/app.js
@@ -1,6 +1,5 @@
-console.log('pew')
-
-var myjson = { };
+// Load our JSON file
+var myjson = {};
 $.ajax({
     url: 'regulation.json',
     dataType: 'json',
@@ -9,36 +8,45 @@ $.ajax({
     }
 });
 
+// Recursively loop through JSON file
 function recursiveLoop(obj) {
   for (var k in obj) {
+    var i = 0;
     if (obj[k].hasOwnProperty("name")) {
       // console.log(obj[k])
-      createElement(obj[k])
+      createDiv(obj[k], i)
       recursiveLoop(obj[k])
     }
     if (obj[k].hasOwnProperty("requirements")) {
       recursiveLoop(obj[k].requirements)
       var child = obj[k].requirements
-      createChildElement(obj[k], child)
+      createChildElement(obj[k], child, i)
     }
     if (typeof obj[k] == "object" && obj[k] !== null){
       recursiveLoop(obj[k])
     }
+    i++
   }
 }
 
-function createElement(obj){
+// Create a new div for each business Rule
+function createDiv(obj, counter){
   text = obj.name
-  $("#list").append("<div class='col-lg-3' id=" + text + ">" + "<h2>" + text + "</h2>" + "</div>")
+  $("#list").append("<div class='col-lg-3'><div class='panel panel-default' id="+ text+"><div class='panel-heading'>" + "<h2 class='panel-title'> <a data-toggle='collapse' href='#" + text+counter + "'>"+ text+ "</a></h2></div>" + "</div>")
 }
 
-function createChildElement(obj, child){
+// Create a new child element for nested properties
+function createChildElement(obj, child, counter){
   var parent = document.getElementById(obj.name)
+  console.log(obj)
+  $(parent).append("<div id="+obj.name+ counter+" class='panel-collapse collapse'><div class='panel-body' id='panel"+obj.name+ counter+"'></div></div></div>")
   for (var key in child) {
     console.log(key)
+    var panel_body_id = "panel" + obj.name+counter
+    var panel_body = document.getElementById(panel_body_id)
     if (child.hasOwnProperty(key)) {
       var text = "<p>" + key + ':' + child[key] + "</p>"
-      $(parent).append(text)
+      $(panel_body).append(text)
     }
   }
 }
