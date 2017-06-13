00acf83526be78dd81e60a28193cd29d57e00727
Tinus Wagner <tinus@3months.com>
2017-06-14 11:43:56 +1200

    filterable list

--- src/javascripts/app.js
+++ src/javascripts/app.js
@@ -1,12 +1,91 @@
 // Load our JSON file
-var myjson = {};
+var myJson = {}
 $.ajax({
     url: 'regulation.json',
     dataType: 'json',
     success: function(json) {
-      recursiveLoop(json)
+      myJson = json
     }
 });
+
+$(function(){
+  $('#fancy-checkbox-childcare').change(
+    function(){
+      if ($(this).is(':checked')) {
+        console.log(myJson)
+        recursiveLoop(getObjects(myJson, 'category', 'hardship'))
+      }
+    }
+  );
+  $('#fancy-checkbox-immigration').change(
+    function(){
+      if ($(this).is(':checked')) {
+        console.log(myJson)
+        recursiveLoop(getObjects(myJson, 'category', 'hardship'))
+      }
+    }
+  );
+  $('#fancy-checkbox-retired').change(
+    function(){
+      if ($(this).is(':checked')) {
+        console.log(myJson)
+        recursiveLoop(getObjects(myJson, 'category', 'retirement'))
+      }
+    }
+  );
+  $('#fancy-checkbox-health').change(
+    function(){
+      if ($(this).is(':checked')) {
+        console.log(myJson)
+        recursiveLoop(getObjects(myJson, 'category', 'hardship'))
+      }
+    }
+  );
+})
+
+function getObjects(obj, key, val) {
+  var objects = [];
+  for (var i in obj) {
+    if (!obj.hasOwnProperty(i)) continue;
+    if (typeof obj[i] == 'object') {
+        objects = objects.concat(getObjects(obj[i], key, val));
+    } else if (i == key && obj[key] == val) {
+        objects.push(obj);
+    }
+  }
+  return objects;
+}
+
+function recursiveFunction(key, val) {
+  actualFunction(key, val);
+  var value = val['businessRules'];
+  console.log(value)
+  if (value instanceof Object) {
+    $.each(value, function(key, val) {
+      console.log(key + val)
+      recursiveFunction(key, val)
+    });
+  }
+}
+function actualFunction(key, val) {
+  // console.log(key,val)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // Recursively loop through JSON file
 function recursiveLoop(obj) {
@@ -21,6 +100,11 @@ function recursiveLoop(obj) {
       var child = obj[k].requirements
       createChildElement(obj[k], child, i)
     }
+    // if (obj[k].hasOwnProperty("alreadyReceiving")) {
+    //   recursiveLoop(obj[k].alreadyReceiving)
+    //   var child = obj[k].alreadyReceiving
+    //   createNestedChildElement(obj[k], child)
+    // }
     if (typeof obj[k] == "object" && obj[k] !== null){
       recursiveLoop(obj[k])
     }
@@ -31,14 +115,16 @@ function recursiveLoop(obj) {
 // Create a new div for each business Rule
 function createDiv(obj, counter){
   text = obj.name
+  category = obj.category
   var title = returnTitle(text)
-  $("#list").append("<div class='col-lg-3'><div class='panel panel-default' id="+ text+"><div class='panel-heading'>" + "<h2 class='panel-title'> <a data-toggle='collapse' href='#" + text+counter + "'>"+ title+ "</a></h2></div>" + "</div>")
+  var type = category
+  $("#list").append("<div class='col-lg-3'><div class='panel panel-default' id="+ text+"><div class='panel-heading'>" + "<div class='row'><div class='col-lg-12'><div class='col-lg-6'><h2 class='panel-title'> <a data-toggle='collapse' href='#" + text+counter + "'>"+ title+ "</a></h2></div><div class='col-lg-6'><button class='btn btn-info'>"+ type +"</button></div></div></div></div></div>")
 }
 
 // Create a new child element for nested properties
 function createChildElement(obj, child, counter){
   var parent = document.getElementById(obj.name)
-  $(parent).append("<div id="+obj.name+ counter+" class='panel-collapse collapse'><div class='panel-body' id='panel"+obj.name+ counter+"'></div></div></div>")
+  $(parent).append("<div id="+obj.name+ counter+" class='panel-collapse collapse'><div class='panel-body text-left' id='panel"+obj.name+ counter+"'></div></div></div>")
   for (var key in child) {
     var panel_body_id = "panel" + obj.name+counter
     var panel_body = document.getElementById(panel_body_id)
@@ -49,6 +135,17 @@ function createChildElement(obj, child, counter){
   }
   var requirementsCount = "<p>" + countRequirements(panel_body) + " requirements</p>"
   $(parent).append(requirementsCount)
+}
+
+function createNestedChildElement(obj, child){
+  console.log(obj)
+  var parent = document.getElementById(obj.name)
+  for (var key in child) {
+    if (child.hasOwnProperty(key))
+    var text = "<p>" + returnRequirementKey(key) + ' :<strong>' + child[key] + "</strong></p>"
+    $(parent).append(text)
+
+  }
 }
 
 function returnTitle(text){
@@ -67,18 +164,7 @@ function returnRequirementKey(text){
 }
 
 function countRequirements(div){
+  // console.log(div)
   var count = document.getElementById(div.id).childElementCount;
   return count
 }
-
-// $(function() {
-//   $.each(obj, function(key, values) {
-//       var $li = $('<li></li>').appendTo(".list"),
-//           $a  = $("<a>", {text: 'Row ' + key}).appendTo($li),
-//           $ol = $('<ol></ol>').appendTo($li);
-//
-//       $.each(values, function(i, val) {
-//          $("<li>", {text: val}).appendTo($ol);
-//       });
-//   });
-// })
