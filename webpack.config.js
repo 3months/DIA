aee99a5e641410b10dc98448b508d8f3b125f2f5
Tinus Wagner <tinus@3months.com>
2017-06-28 10:01:14 +1200

    updated webpack config with mustach loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,32 @@
 var path = require('path');
+var webpack = require('webpack');
 
 module.exports = {
+  devtool: 'cheap-module-source-map',
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      sourceMap: true,
+      compress: {
+        warnings: false,
+        pure_getters: true,
+        unsafe: true,
+        unsafe_comps: true,
+        screw_ie8: true
+      },
+      output: {
+        comments: false,
+      }
+    }),
+    new webpack.optimize.AggressiveMergingPlugin()
+  ],
   entry: './src/javascripts/app.js',
   output: {
-    path: __dirname + '/dist',
+    path: __dirname + '/dist/',
     filename: 'bundle.js'
   },
   module: {
@@ -44,6 +67,10 @@ module.exports = {
       {
         test: /\.(png|woff|woff2|eot|ttf|svg)$/,
         loader: 'url-loader?limit=100000'
+      },
+      {
+        test: /\.html$/,
+        loader: 'mustache-loader'
       }
     ]
   }
